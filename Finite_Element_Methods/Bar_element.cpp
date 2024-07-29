c0bfd00b98741e7a34f20e1bd3ba042cc3a41faf
puneeth dambe <puneethdambe@outlook.com>
2024-07-30 00:18:38 +0530

    Local stiffness matrix added

--- Finite_Element_Methods/Bar_element.cpp
+++ Finite_Element_Methods/Bar_element.cpp
@@ -1,8 +1,45 @@
+// Code to solve finite element problem for bar element
+
+// Author: PUNEETHA KUMAR D
+//MTech in Mechanical Engineering IISc Bangalore
+// Date: 29/ 07 / 2024
+
+
+
 #include<iostream>
 
+// Local stiffness matrix for bar element
 
-int main() {
-	std::cout << "hello";
-	return 0;
+void local_stiffness_matrix() 
+{
+	// Declare 2D array
+	void stiffness_matrix[2][2];
 
+	for (int i = 0; i < 2; i++) 
+	{
+		for (int j = 0; j < 2; j++) 
+		{
+			if (i == j) 
+			{
+				stiffness_matrix[i][j] = 1;
+			}
+			if (j == i + 1 || j == i - 1) 
+			{
+				stiffness_matrix[i][j] = -1;
+			}
+			else 
+			{
+				stiffness_matrix[i][j] = 0;
+
+			}
+
+		}
+
+	}
+	return stiffness_matrix;
 }
+
+int main(){
+		local_stiffness_matrix();
+
+	};
