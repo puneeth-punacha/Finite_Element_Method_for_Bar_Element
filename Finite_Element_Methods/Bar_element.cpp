2e5367906e17b11c051b86dbe087a0e26c4eaf5a
puneeth dambe <puneethdambe@outlook.com>
2024-07-30 22:39:41 +0530

    Started global stiffness matrix

--- Finite_Element_Methods/Bar_element.cpp
+++ Finite_Element_Methods/Bar_element.cpp
@@ -31,6 +31,10 @@ std::vector < std::vector<int>> local_stiffness_matrix(int size)
 	return matrix;
 }
 
+// Function to Global stiffness matrix for bar element
+
+std:: 
+
 /*int local_stiffness_matrix()
 {
 	// Declare 2D array
@@ -62,19 +66,41 @@ std::vector < std::vector<int>> local_stiffness_matrix(int size)
 */
 
 
-int main(){
-		//local_stiffness_matrix();
-		std::vector< std::vector<int>> new_matrix = local_stiffness_matrix(5);
+int main()
+{
 
-		// Print the indentity matrix
-		for (const auto& row: new_matrix) 
+	//local_stiffness_matrix();
+	
+	// For bar element n is 2 for local stiffness matrix
+	int n; 
+	n = 2;  
+
+	// Local striffness matrix
+	std::vector< std::vector<int>> new_matrix = local_stiffness_matrix(n);
+	
+	// Print the  Local striffness matrix
+	for (const auto& row: new_matrix) 
+	{
+		for (int element : row)
 		{
-			for (int element : row)
-			{
-				std::cout << element << " ";
-			}
-			std::cout << std::endl;
-
+			std::cout << element << " ";
 		}
+		std::cout << std::endl;
+	}
+
+	// Global Stiffness matrix
+	/*
+	Global stiffness matrix is obtained by assembling the local stiffness matrix for each element.
+	
+	The dimension of global stiffness matrix depends on number of nodes in whole 
+	model and in degrees of freedom of each element.
+
+	size of globle stiffness matrix = Number of nodes in model *  degrees of freedom of each element */
+
+	int number_of_nodes_in_model = 2;
+	int Degrees_of_freedom_of_each_element = 1; // For bar element the displacement specifies the state of node. 
+
+	int size_of_global_stiffness_matrix = number_of_nodes_in_model * Degrees_of_freedom_of_each_element;
+	std::vector <std::vector<int>> Global_stiffness_matrix = Formation_of_global_stiffness_matrix(size_of_global_stiffness_matrix, local_stiffness_matrix);
 
 	};
